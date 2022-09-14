ab84f955dcba7ce962c2c42c728e8e33e850c1d6
ar <ar.com>
2022-09-14 20:12:09 +0800

    9.14

--- lodash/arlalala-lodish.js
+++ lodash/arlalala-lodish.js
@@ -1,7 +1,21 @@
 
 var arlalala= {
 
-chunk: function() {},
+  chunk: function (nums, k) {
+    if (nums.length <= k) {
+      return nums
+    } else {
+      var numsa = []
+      for (var i = 0; i < nums.length; i = i + k){
+           var  n = i / k
+           var numsa[n] = []
+        for (var j = 0; j < k; j++){
+           numsa[n][j] = nums[j +k * i]
+        }
+      }
+    }
+    return numsa
+},
 
 compact: function() {},
 
